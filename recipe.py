10767789d490773e0739c338b4307f8c2860eb0a
Zhang Shirley <shirl@Shirleys-Mac.local>
2020-04-13 14:31:46 -0400

    initiating

--- recipe.py
+++ recipe.py
@@ -4,8 +4,10 @@ import os
 import requests
 
 def searchProduct(product):
+    app_id = '15607767'
+    app_key = '866dcff09fa31e2e0e2249d52177fe20	'
     try:
-        request_url = 'https://api.edamam.com/search?q=chicken&app_id=15607767&app_key=866dcff09fa31e2e0e2249d52177fe20&from=0&to=3&calories=591-722&health=alcohol-free'
+        request_url = 'https://api.edamam.com/search?q=chicken&app_id={}&app_key={}&from=0&to=3&calories=591-722&health=alcohol-free'.format(app_id, app_key)
         r = requests.get(request_url)
         d1 = json.loads(r.text) 
     except:
