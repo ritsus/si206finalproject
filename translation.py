69068b910ae5d566316e008e23d576acc0f5b5f2
Natàlie Lillig <natalielillig@natalies-mbp-2.lan>
2020-04-24 13:48:53 -0700

    slightly edited

--- translation.py
+++ translation.py
@@ -6,9 +6,34 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+
+
+querystring = {"source":"{source}","target":"{target}","input":"{input}"}
+
+headers = {
+    'x-rapidapi-host': "systran-systran-platform-for-language-processing-v1.p.rapidapi.com",
+    'x-rapidapi-key': "86e1254777msh4331a57cf3bc755p11f980jsnceff71f415ab"
+    }
+
+
+
 # API https://rapidapi.com/systran/api/systran-io-translation-and-nlp
 def translator_url()
     key = "86e1254777msh4331a57cf3bc755p11f980jsnceff71f415ab"
     key_url = "apiKey=" + key
-    url = "https://rapidapi.com/systran/api/systran-io-translation-and-nlp" + key_url
+    url = "https://systran-systran-platform-for-language-processing-v1.p.rapidapi.com/translation/text/translate"
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    print(response.text)
     return url  
+
+
+
+
+
+
+def main():
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+'Food Data' + ".db")
+    cur = conn.cursor()
+if __name__ == "__main__":
+    main()
