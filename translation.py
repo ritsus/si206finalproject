11f54810c9630a7df50b25292a7b82250b4b20cc
Natàlie Lillig <natalielillig@natalies-mbp-2.lan>
2020-04-24 23:48:06 -0700

    refer to previous commit for comments, edited the while loop and the language lower for inputs

--- translation.py
+++ translation.py
@@ -18,14 +18,17 @@ def translateText(targetLanCode, text):
 
 def languages()
     Language = input(“Enter translated language:”)
+    Language.lower() 
     #only the top 50 langauges used in the world are available 
     Lang_abr = {'zh': 'chi', 'english' : 'en', 'spanish': 'en', 'arabic': 'ar', 'begali': 'bn', 'hindi': 'hi', 'russian' : 'ru', 'portuguese': 'pt',
         'japanese': 'ja','german': 'de', 'Javanese':'jv', 'korean': 'ko','french':'fr', 'turkish':'tr', 'vietnamese': 'vi', 'Telugu':'te', 'marathi': 'mr',
         'tamil':'ta', 'italian': 'it', 'urdu': 'ur', 'guarati': 'gu', 'polish': 'pl', 'ukrainian': 'uk', 'persian': 'fa', 'malay':'ms', 'kannada':'kn', 'oriya':'or',
         'punjabi': 'pa', 'sunda': 'su', 'romanian': 'rm', 'bhojpuri':'bh', 'azerbaijani': 'az', 'maithili': 'bh', 'hausa': 'ha', 'burmese': 'my', 'serbian': 'sr',
         'thai': 'th', 'dutch':'nl', 'yoruba': 'yo', 'sindhi': 'sd', 'slovak':'sk', 'swahili':'sw'}
+        #taken from https://photius.com/rankings/languages2.html
 
-    while 1: 
+    
+    while True: 
         if Language in Lang_abr:
             abr = Lang_abr[Language]
             #insert the function or text that will be translated where text is 
