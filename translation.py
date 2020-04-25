f7ed6f9a37e2757231545b9390467c77ac3e4b94
Natàlie Lillig <natalielillig@natalies-mbp-2.lan>
2020-04-24 23:42:55 -0700

    line 31 is where it needs to be altered. if you want to test the API, uncomment line 17 and comment def languages. I did not know what we were inputting

--- translation.py
+++ translation.py
@@ -14,9 +14,24 @@ def translateText(targetLanCode, text):
   resp = requests.get('https://translation.googleapis.com/language/translate/v2?target={}&key={}&q={}'.format(targetLanCode, API_KEY, text))
   return resp.json()['data']['translations'][0]['translatedText']
 
-print(translateText('es', 'I like cookies'))
+  #print(translateText('es', 'I like cookies'))
 
+def languages()
+    Language = input(“Enter translated language:”)
+    #only the top 50 langauges used in the world are available 
+    Lang_abr = {'zh': 'chi', 'english' : 'en', 'spanish': 'en', 'arabic': 'ar', 'begali': 'bn', 'hindi': 'hi', 'russian' : 'ru', 'portuguese': 'pt',
+        'japanese': 'ja','german': 'de', 'Javanese':'jv', 'korean': 'ko','french':'fr', 'turkish':'tr', 'vietnamese': 'vi', 'Telugu':'te', 'marathi': 'mr',
+        'tamil':'ta', 'italian': 'it', 'urdu': 'ur', 'guarati': 'gu', 'polish': 'pl', 'ukrainian': 'uk', 'persian': 'fa', 'malay':'ms', 'kannada':'kn', 'oriya':'or',
+        'punjabi': 'pa', 'sunda': 'su', 'romanian': 'rm', 'bhojpuri':'bh', 'azerbaijani': 'az', 'maithili': 'bh', 'hausa': 'ha', 'burmese': 'my', 'serbian': 'sr',
+        'thai': 'th', 'dutch':'nl', 'yoruba': 'yo', 'sindhi': 'sd', 'slovak':'sk', 'swahili':'sw'}
 
+    while 1: 
+        if Language in Lang_abr:
+            abr = Lang_abr[Language]
+            #insert the function or text that will be translated where text is 
+            print(translateText(abr,#text)) 
+            break 
+        else input("That is not a valid language. Try again: "):
 
 
 
